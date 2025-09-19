1641b53e166687fbea5caee10e9b6ff2bc9d58d1
Dmitry Bukin <bukind@indoaustinvestments.com>
2025-09-19 20:26:18 +1000

    A new attempt to make keep-alive work, failed

--- bme280/main/i2c_basic_example_main.c
+++ bme280/main/i2c_basic_example_main.c
@@ -91,15 +91,16 @@ void app_main(void)
     char response_buf[512+1];
     httpcli_user_data_t user_data = HTTPCLI_USER_DATA_INIT(response_buf, sizeof(response_buf));
     ESP_LOGI(TAG, "user_data has: buf=%x buflen=%d gotlen=%d", user_data.buffer, user_data.buflen, user_data.gotlen);
-    const char* url = "http://192.168.5.3:8080/";
+    const char* url = "http://192.168.5.2:8080/tmp.txt";
+    esp_http_client_method_t method = HTTP_METHOD_GET;
     esp_http_client_config_t http_config = {
         .url = url,
-        .method = HTTP_METHOD_GET,
+        .method = method,
         .event_handler = httpcli_event_handler,
         .user_data = &user_data,
         .keep_alive_enable = true,
         .keep_alive_idle = 5,
-        .keep_alive_interval = 20,
+        .keep_alive_interval = 5,
         .keep_alive_count = 3,
     };
     esp_http_client_handle_t client = esp_http_client_init(&http_config);
@@ -110,7 +111,7 @@ void app_main(void)
     ESP_LOGI(TAG, "Initialized HTTP client with user data @ %x", &user_data);
     err = esp_http_client_set_header(client, "Connection", "keep-alive");
     ESP_LOGI(TAG, "set connection header: (%d) %s", DBGERR(err));
-    esp_http_client_set_header(client, "Keep-Alive", "timeout=20, max=10");
+    esp_http_client_set_header(client, "Keep-Alive", "timeout=60, max=10");
     ESP_LOGI(TAG, "set keep-alive header: (%d) %s", DBGERR(err));
 
     err = esp_http_client_perform(client);
@@ -118,17 +119,21 @@ void app_main(void)
     ESP_LOGI(TAG, "user_data has: buf=%x buflen=%d gotlen=%d", user_data.buffer, user_data.buflen, user_data.gotlen);
     ESP_LOG_BUFFER_HEX(TAG, user_data.buffer, user_data.gotlen);
 
+    /*
     for (int i = 0; i < 10; i++) {
         ESP_ERROR_CHECK(bme280_measure_once(sensor));
         usleep(1000000);
     }
+     */
+    sleep(10);
 
     // Reset the response.
     user_data.gotlen = 0;
     // The second call does not work yet!
-    err = esp_http_client_set_url(client, url);
+    const char* url2 = "http://192.168.5.2:8080/tmp2.txt";
+    err = esp_http_client_set_url(client, url2);
     ESP_LOGI(TAG, "Set URL: (%d) %s", DBGERR(err));
-    err = esp_http_client_set_method(client, HTTP_METHOD_GET);
+    err = esp_http_client_set_method(client, method);
     ESP_LOGI(TAG, "Set method: (%d) %s", DBGERR(err));
     err = esp_http_client_perform(client);
     ESP_LOGI(TAG, "HTTP perform: (%d) %s", DBGERR(err));
